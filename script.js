63884c7cc713bc896d26deb062b96f5c87ee0655
pokechirlia <chi.k.tran2000@gmail.com>
2022-11-26 12:34:30 +1100

    add play function
    basically the game can be played but no checking

--- script.js
+++ script.js
@@ -11,10 +11,26 @@ const PLAYER_O = 'O';
 let is_X_turn = true;
 
 //set up functions
-options.forEach((option) => option.addEventListener('click', run));
+options.forEach((option) => option.addEventListener('click', makeMove));
 restart_button.addEventListener('click', run);
 
 function run(){
     console.log("a");
+}
+
+function makeMove(event){
+
+    const option = event.target;
+    
+    if(is_X_turn){
+        option.innerText = PLAYER_X;
+        is_X_turn = false;
+    }
+
+    else{
+        option.innerText = PLAYER_O;
+        is_X_turn = true;
+    }
+
 }
 
