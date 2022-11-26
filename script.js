6c19e8ff44dc00ec63b441c4296afdf1887a8a3a
pokechirlia <chi.k.tran2000@gmail.com>
2022-11-26 13:30:33 +1100

    display game over when winner is found

--- script.js
+++ script.js
@@ -9,14 +9,51 @@ const restart_button = document.getElementById("restart-button");
 const PLAYER_X = 'X';
 const PLAYER_O = 'O';
 let is_X_turn = true;
+let player_X_record = [];
+let player_O_record = [];   
+const winning_cases = new Map([      //all winning cases with the corresponding strike type
+    [[1,2,3], "strike-row-1"],
+    [[4,5,6], "strike-row-2"],
+    [[7,8,9], "strike-row-3"],
+
+    [[1,4,7], "strike-column-1"],
+    [[2,5,8], "strike-column-2"],
+    [[7,8,9], "strike-column-3"],
+
+    [[1,5,9], "strike-diagonal-1"],
+    [[3,5,7], "strike-diagonal-2"]
+    
+]);
 
 //set up functions
 options.forEach((option) => option.addEventListener('click', makeMove));
-restart_button.addEventListener('click', run);
+restart_button.addEventListener('click', newGame);
 
 function run(){
     console.log("a");
 }
+
+function setHover(){
+
+//remove all X/O hover and change to the current one
+
+    options.forEach((option) => {
+        option.classList.remove("x-hover");
+        option.classList.remove("o-hover");
+    })
+
+    options.forEach((option) => {
+        if(option.innerHTML == "")
+        {
+            if(is_X_turn)
+                option.classList.add("x-hover");
+            else
+                option.classList.add("o-hover");
+        }
+    })
+}
+
+
 
 function makeMove(event){
 
@@ -25,12 +62,59 @@ function makeMove(event){
     if(is_X_turn){
         option.innerText = PLAYER_X;
         is_X_turn = false;
+        player_X_record.push(parseInt(option.dataset.index));
     }
 
     else{
         option.innerText = PLAYER_O;
         is_X_turn = true;
+        player_O_record.push(parseInt(option.dataset.index));
     }
 
+    checkWinner();
+    setHover();
+
+    
+
+}
+
+function checkWinner(){
+    //console.log(player_X_record);
+    //console.log(player_O_record);
+
+    //X just done their turn
+    if(!is_X_turn)
+    {
+        for(const winningCase of winning_cases.keys()){
+            const result = winningCase.every(val => player_X_record.includes(val));
+            if(result)
+            {
+                gameOver();
+                return;
+            }
+        }
+    }
+
+    else{
+        for(const winningCase of winning_cases.keys()){
+            const result = winningCase.every(val => player_O_record.includes(val));
+            if(result)
+            {
+                gameOver();
+                return;
+            }
+        }
+
+    }
+
+
+}
+
+function newGame(){
+    setHover();
+}
+
+function gameOver(){
+    message_box.className = "visible";
 }
 
